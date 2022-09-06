034c574446987f68ce29928faf2e36f77a5e86e6
Julien Lavocat <julien.lavocat@gmail.com>
2022-09-06 22:41:44 +0200

    feat: add french translation

--- public/src/i18n.ts
+++ public/src/i18n.ts
@@ -1,11 +1,16 @@
 import i18n from "i18next";
-import detector from "i18next-browser-languagedetector";
+import LanguageDetector from "i18next-browser-languagedetector";
 import backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(detector)
+i18n.use(LanguageDetector)
 	.use(backend)
 	.use(initReactI18next)
-	.init({ load: "languageOnly", fallbackLng: "en" });
+	.init({
+		load: "languageOnly",
+		detection: {
+			order: ["navigator"],
+		},
+	});
 
 export default i18n;
